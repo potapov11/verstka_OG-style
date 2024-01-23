5ed39de957cc0c6e35065b3dce25313267c7b61c
Serezha <volos11223344@yandex.ru>
2024-01-23 21:40:12 +0300

    добавлен слайдер

---
+++ src/js/components/slider.js
@@ -0,0 +1,17 @@
+import { auto } from "@popperjs/core";
+
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: auto,
+  spaceBetween: 30,
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
