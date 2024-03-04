b62dfa844aefad520c94a3d1690f1b46750758d7
Serezha <volos11223344@yandex.ru>
2024-03-04 21:45:08 +0300

    работает над адаптив mobile

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -1,8 +1,17 @@
 const myMegaMenu = document.querySelector(".mega_menu");
 const mySpans = document.querySelectorAll(".mega_menu span");
+const nav = document.querySelector(".header__nav");
 let move = false;
 
 myMegaMenu.addEventListener("click", () => {
+  console.log(nav.style.display);
+
+  nav.classList.toggle("show");
+
+  // if (nav.style.display === "none") {
+  //   nav.style.display = "block";
+  // }
+
   if (!move) {
     mySpans[1].classList.add("rmSecond");
     mySpans[0].classList.add("centerizing", "rotF");
